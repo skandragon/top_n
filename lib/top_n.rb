d6abfe50b0e0187425db1854f565c9484b851951
Michael Graff <explorer@flame.org>
2013-12-15 09:24:46 -0600

    start development; add creation tests

--- lib/top_n.rb
+++ lib/top_n.rb
@@ -1,6 +1,16 @@
 ##
 # This class tracks the top (or bottom) N values added.  Discard the rest.
 class TopN
+  attr_reader :maxsize
+  attr_reader :direction
 
+  def initialize(options = {})
+    options = {
+      maxsize: 100,
+      direction: :top,
+    }.merge(options)
 
+    @maxsize = options[:maxsize]
+    @direction = options[:direction]
+  end
 end
--- test/test_top_n.rb
+++ test/test_top_n.rb
@@ -1,7 +1,19 @@
 require 'helper'
 
 class TestTopN < Minitest::Test
-  def test_creation_without_arguments_fails
-    TopN.new
+  def test_creation_without_arguments_works
+    topn = TopN.new
+    assert topn
+    assert topn.maxsize > 0
+  end
+
+  def test_creaton_with_maxsize_works
+    topn = TopN.new(maxsize: 100)
+    assert topn.maxsize == 100
+  end
+
+  def test_creation_with_direction_works
+    topn = TopN.new(direction: :bottom)
+    assert topn.direction == :bottom
   end
 end
